ce5494d1c69c6098c66e400f3c8427c631a3d50f
김혜원 <kihwn98@gmail.com>
2024-08-01 11:02:11 +0900

    feat:0731 과제 추가

--- day03_0731수/src/SWEA_1215_회문1/Solution.java
+++ day03_0731수/src/SWEA_1215_회문1/Solution.java
@@ -1,25 +1,87 @@
 package SWEA_1215_회문1;
+
 import java.util.Scanner;
+
 public class Solution {
+    static String[][]map = new String[8][8];
+    static Scanner sc = new Scanner(System.in);
+    static int cnt = 0;
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
 
-        for (int p=0;  p<10; p++){
-            int n = sc.nextInt();
+        for (int round=0; round<10; round++){
+            cnt =0;
+            int find = sc.nextInt();
             sc.nextLine();
-            String[][]map = new String[8][8];
 
-            for (int i=0; i<8; i++){
-                String s = sc.nextLine();
-                String[]sarr = s.split("");
-                for (int j=0; j<8; j++){
-                    map[i][j]=sarr[j];
-                }
-            }
+            makeMap();
+            checkRow(find);
+            checkCol(find);
 
-            for (int i=0; i<8; i++){}
-
+            int printRound = round+1;
+            System.out.println("#"+printRound+" "+cnt);
 
         }
     }
+
+    public static void makeMap(){
+        for (int i=0; i<8; i++){
+            String s = sc.nextLine();
+            String[]sarr = s.split("");
+            for (int j=0; j<8; j++){
+                map[i][j]=sarr[j];
+            }
+        }
+    }
+
+    public static boolean checkPalin1(int i,int j, int size){
+
+        int round = size/2;
+        boolean check = true;
+
+        for (int z=0; z<round; z++){
+
+            if (!map[i][j+z].equals(map[i][j+size-1-z])){
+                check = false;
+                break;
+            }
+        }
+        return check;
+    }
+
+    public static boolean checkPalin2(int i,int j, int size){
+
+        int round = size/2;
+        boolean check = true;
+
+        for (int z=0; z<round; z++){
+            if (!map[i+z][j].equals(map[size-1+i-z][j])){
+                check = false;
+                break;
+            }
+        }
+        return check;
+    }
+
+    public static void checkRow(int find){
+        for (int i=0; i<=8-find; i++){
+            for (int j=0; j<8; j++){
+                boolean check = checkPalin2(i,j,find);
+                if(check){
+                    cnt++;
+                }
+            }
+        }
+    }
+
+    public static void checkCol(int find){
+        for (int i=0; i<8; i++){
+            for (int j=0; j<=8-find; j++){
+                boolean check = checkPalin1(i,j,find);
+                if(check){
+                    cnt++;
+                }
+            }
+        }
+    }
+
 }
--- day03_0731수/src/SWEA_1989_초심자의외문검사/Solution.java
+++ day03_0731수/src/SWEA_1989_초심자의외문검사/Solution.java
@@ -1,5 +1,35 @@
 package SWEA_1989_초심자의외문검사;
 
+import java.util.Scanner;
+
 public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        for (int i=0; i<n; i++){
+            String s = sc.nextLine();
+            String[]sarr = s.split("");
+            int size = sarr.length;
+            boolean check = true;
+            for (int j=0; j<size/2; j++){
+                if (!sarr[j].equals(sarr[size-1-j])){
+                    check = false;
+                    break;
+                }
+            }
+
+            int round = i+1;
+
+            if (check){
+                System.out.println("#"+round+" 1");
+            }
+            else{
+                System.out.println("#"+round+" 0");
+            }
+        }
+
+
+    }
 
 }
